bae9384f03f573826d45da42dbca54daa2ada02e
Prelang Builder <builder@prelang.com>
2014-09-21 18:27:53 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/note_cards_controller.rb
+++ app/controllers/note_cards_controller.rb
@@ -1,4 +1,8 @@
 class NoteCardsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_note_card, only: [:show, :edit, :update, :destroy]
 
   # GET /note_cards
