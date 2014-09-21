95540312c76736bce5f2a4a5ccdbe114ec3b5ef5
Prelang Builder <builder@prelang.com>
2014-09-21 18:28:06 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/note_cards_controller.rb
+++ app/controllers/note_cards_controller.rb
@@ -3,7 +3,7 @@ class NoteCardsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_note_card, only: [:show, :edit, :update, :destroy]
+  before_action :set_note_card, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /note_cards
   # GET /note_cards.json
